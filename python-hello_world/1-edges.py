38581707d2f746b0d7ea5df214c17f5f5ad3037e
DeveloperTum <devlopertum@gmail.com>
2023-07-20 15:43:31 +0300

    variable update

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,7 +2,7 @@ word = "Holberton"
 print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[7:]))
 print("Middle word: {}".format(word[1:8]))
-word = "School"
-print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[4:]))
-print("Middle word: {}".format(word[1:5]))
+t = "School"
+print("First 3 letters: {}".format(t[0:3]))
+print("Last 2 letters: {}".format(t[4:]))
+print("Middle word: {}".format(t[1:5]))
