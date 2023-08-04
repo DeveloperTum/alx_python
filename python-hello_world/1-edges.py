c0f40d39592a82564d20142f8e9f9a52f86207dc
DeveloperTum <devlopertum@gmail.com>
2023-08-04 22:26:32 +0300

    playing with my slicing

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word = "Holberton"
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1:-1]))
 word = "School"
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[4:]))
-print("Middle word: {}".format(word[1:5]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1:-1]))
