cfe00f04a452190eca32148a81088982f4e28651
DeveloperTum <devlopertum@gmail.com>
2023-08-08 18:24:13 +0300

    last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,8 +10,7 @@ if number>=0:
         print("Last digit of",number,"is",last,"and is 0")
     else:
         print("Last digit of",number,"is",last,"and is less than 6 and not 0")
-else:
-    if number < 0:
+elif number < 0:
         print("Last digit of",number,"is",last,"and is less than 6 and not 0")
 
     
